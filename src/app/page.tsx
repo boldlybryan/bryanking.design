7f27f7fcbc545958f282a1b0a1cc280a83516d5d
Bryan King <bpking15@gmail.com>
2025-12-03 11:56:17 -0500

    Update grid layout in Home component for better responsiveness on smaller screens.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <p>Software Design & Engineering</p>
         <p>Newport, Kentucky, USA</p>
       </div>
-      <div className="grid grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <div className="border w-full aspect-16/10"></div>
         <div className="border w-full aspect-16/10"></div>
         <div className="border w-full aspect-16/10"></div>
