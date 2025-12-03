0e55135f31f567154430679b75a101252b3dfafd
Bryan King <bpking15@gmail.com>
2025-12-03 11:48:53 -0500

    Enhance Home component layout with improved styling and additional grid structure for content display.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 export default function Home() {
   return (
-    <main className="">
-      <h1>Bryan King</h1>
-      <p>Software Design & Engineering</p>
-      <p>Newport, KY</p>
+    <main className="p-4 font-medium leading-5">
+      <div className="mb-12">
+        <h1>Bryan King</h1>
+        <p>Software Design & Engineering</p>
+        <p>Newport, Kentucky, USA</p>
+      </div>
+      <div className="grid grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+        <div className="border w-full aspect-16/10"></div>
+      </div>
     </main>
   );
 }
