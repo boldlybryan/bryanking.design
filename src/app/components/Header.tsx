cca9ca3ff4db05b0463f2d1b70aaa3987fd3f2c4
Bryan King <bpking15@gmail.com>
2025-12-07 15:07:23 -0500

    Refactor Header component: wrap content in a header tag for semantic structure, and add social media links for improved connectivity and user engagement.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,12 +2,19 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <Link href="/">
-      <div className="mb-12">
-        <h1>Bryan King</h1>
-        <p>Software Design & Engineering</p>
-        <p>Newport, Kentucky, USA</p>
+    <header className="mb-8">
+      <Link href="/">
+        <div className="">
+          <h1>Bryan King</h1>
+          <p>Software Design & Engineering</p>
+          <p>Newport, Kentucky, USA</p>
+        </div>
+      </Link>
+      <div className="flex gap-4 mt-4">
+        <Link href="https://twitter.com/bryan_king" target="_blank">Twitter</Link>
+        <Link href="https://linkedin.com/in/bpking15" target="_blank">LinkedIn</Link>
+        <Link href="https://github.com/boldlybryan" target="_blank">GitHub</Link>
       </div>
-    </Link>
+    </header>
   );
 }
