79145593513ce06d87f0f007f00831d0fc97b9e5
Bryan King <bpking15@gmail.com>
2025-12-08 12:02:31 -0500

    Update Footer component: change Experiments link to point to the correct route and add a new Blog link that opens in a new tab.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,8 @@ export default function Footer() {
  return (
     <footer>
       <nav className="flex gap-4">
-        <Link href="#">Experiments</Link>
+        <Link href="/experiments">Experiments</Link>
+        <Link href="https://bryanking.net" target="_blank">Blog</Link>
         <Link href="/colophon">Colophon</Link>
       </nav>
     </footer>
