d97b3848accdec31b44e5243e088a6c35d23cc89
Bryan King <bpking15@gmail.com>
2025-12-08 13:45:07 -0500

    Update Footer component: enhance navigation by adding Newsletter, Résumé, and Are.na links, and adjust layout classes for improved responsiveness.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 
 export default function Footer() {
  return (
-    <footer>
-      <nav className="flex gap-4">
+    <footer className="grid-home">
+      <nav className="xl:col-start-3 col-span-2 flex gap-4 overflow-x-auto">
+        <Link href="/colophon">Colophon</Link>
         <Link href="/experiments">Experiments</Link>
         <Link href="https://bryanking.net" target="_blank">Blog</Link>
-        <Link href="/colophon">Colophon</Link>
+        <Link href="https://www.bryanking.net/newsletter" target="_blank">Newsletter</Link>
+        <Link href="#" target="_blank">Résumé</Link>
+        <Link href="https://are.na/bryan-king" target="_blank">Are.na</Link>
       </nav>
     </footer>
  )
