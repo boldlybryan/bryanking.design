0bc823db65905dcd90557eabe5c0baedca6a81f7
Bryan King <bpking15@gmail.com>
2025-12-08 13:46:21 -0500

    Add résumé link to Footer component and include Bryan King's resume PDF

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <Link href="/experiments">Experiments</Link>
         <Link href="https://bryanking.net" target="_blank">Blog</Link>
         <Link href="https://www.bryanking.net/newsletter" target="_blank">Newsletter</Link>
-        <Link href="#" target="_blank">Résumé</Link>
+        <Link href="/files/bryan-king-resume.pdf" target="_blank">Résumé</Link>
         <Link href="https://are.na/bryan-king" target="_blank">Are.na</Link>
       </nav>
     </footer>
