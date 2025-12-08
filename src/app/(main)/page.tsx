9fc8f9cc21baae28fd159025baa2adb3d7dc5f84
Bryan King <bpking15@gmail.com>
2025-12-07 21:08:27 -0500

    Update Home component layout: increase margin for headings and adjust border colors for improved visual hierarchy and consistency across project sections.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
       </div>
       <div>
         <div className="flex gap-2 items-baseline">
-          <h2 className="title mb-2">NaviStone, Inc</h2>
+          <h2 className="title mb-6">NaviStone, Inc</h2>
           <p className="opacity-75">(2022–present)</p>
         </div>
-        <div className="grid-home mb-8 pb-8 border-b border-neutral-700">
+        <div className="grid-home mb-8 pb-10 border-b border-neutral-800">
           <CasePreview
             title="Next-gen Prototype"
             description="A Ruby on Rails proof-of-concept to align the business for on the next 10 years"
@@ -46,10 +46,10 @@ export default function Home() {
       </div>
       <div>
         <div className="flex gap-2 items-baseline">
-          <h2 className="title mb-2">Intuit MailChimp</h2>
+          <h2 className="title mb-6">Intuit MailChimp</h2>
           <p className="opacity-75">(2021–2022)</p>
         </div>
-        <div className="grid-home mb-8 pb-8 border-b border-neutral-700">
+        <div className="grid-home mb-8 pb-10 border-b border-neutral-800">
           <CasePreview
             title="Custom Reports Prototype"
             description="Lorem ipsum dolor"
@@ -78,10 +78,10 @@ export default function Home() {
       </div>
       <div>
         <div className="flex gap-2 items-baseline">
-          <h2 className="title mb-2">Flagstar Bank</h2>
+          <h2 className="title mb-6">Flagstar Bank</h2>
           <p className="opacity-75">(2015–2021)</p>
         </div>
-        <div className="grid-home mb-8 pb-8 border-b border-neutral-700">
+        <div className="grid-home mb-8 pb-10 border-b border-neutral-800">
           <CasePreview
             title="Flagstar.com Redesign"
             description="Lorem ipsum dolor & accessibility overhaul"
@@ -109,22 +109,22 @@ export default function Home() {
         </div>
       </div>
       <div className="">
-        <h2 className="title mb-2">Extracurriculars</h2>
-        <div className="grid-home">
-          <div className="border border-neutral-500 w-full aspect-16/10 object-fill">
+        <h2 className="title mb-6">Extracurriculars</h2>
+        <div className="grid-home mb-8 pb-10">
+          <div className="border border-neutral-800 w-full aspect-16/10 object-fill">
             <Link href="https://stendigcal.vercel.app" target="_blank">
               <Image className="h-full" src="/thumbnails/stendig-calendar.gif" alt="" width={1000} height={1000} />
             </Link>
           </div>
-          <div className="border border-neutral-500 w-full aspect-16/10  cursor-pointer">
+          <div className="border border-neutral-700 w-full aspect-16/10  cursor-pointer">
             <Link href="https://www.bryanking.net/posts/no-more-hog-butchering" className="h-full w-full flex items-center justify-center">No More Hog Butchering</Link>
           </div>
-          <div className="border border-neutral-500 w-full aspect-16/10  cursor-pointer">
+          <div className="border border-neutral-700 w-full aspect-16/10  cursor-pointer">
             <Link href="https://codepen.io/brykng/pen/oywrLv" className="h-full w-full flex items-center justify-center">
             <Image className="h-full" src="/thumbnails/stadttheatre.gif" alt="" width={1000} height={1000} />
             </Link>
           </div>  
-          <div className="border border-neutral-500 w-full aspect-16/10  cursor-pointer">
+          <div className="border border-neutral-700 w-full aspect-16/10  cursor-pointer">
             <Link href="https://codepen.io/collection/AZyZMQ?cursor=eyJwYWdlIjoxfQ==" className="h-full w-full flex items-center justify-center">
               CodePen Cincinnati Presentations
             </Link>
