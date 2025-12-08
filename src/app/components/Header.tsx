5c86b84336add03167cc24b37ee731a11c02f09b
Bryan King <bpking15@gmail.com>
2025-12-07 20:50:54 -0500

    Refactor Header component layout: enhance navigation structure by adding new links and adjusting flex properties for improved organization and responsiveness.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="container mb-8">
+    <header className="container mb-8 grid-home border-b border-neutral-800 pb-4">
       <Link href="/">
         <div className="">
           <h1>Bryan King</h1>
@@ -10,7 +10,12 @@ export default function Header() {
           <p>Newport, Kentucky, USA</p>
         </div>
       </Link>
-      <div className="flex gap-4 mt-4">
+      <div className="flex flex-col xl:col-start-3">
+        <Link href="#">Method</Link>
+        <Link href="#">History</Link>
+        <Link href="#">Influences</Link>
+      </div>
+      <div className="flex flex-col">
         <Link href="https://twitter.com/bryan_king" target="_blank">Twitter</Link>
         <Link href="https://linkedin.com/in/bpking15" target="_blank">LinkedIn</Link>
         <Link href="https://github.com/boldlybryan" target="_blank">GitHub</Link>
