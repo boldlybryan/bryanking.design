65d33263c890ac0e96e95358b61b48106b67ddbc
Bryan King <bpking15@gmail.com>
2025-12-10 16:13:32 -0500

    Add new pages for Flagstar, Mailchimp, and Navistone projects: create dedicated components for FlagstarPage, AnalyticsDashboardPage, CustomReportsPage, NavistonePage, AnalyticsPage, IrisDesignSystemPage, and NextGenPlatformPrototypePage, enhancing project documentation and user navigation.

---
+++ src/app/(main)/(content)/work/flagstar/page.tsx
@@ -0,0 +1,17 @@
+export default function FlagstarPage() {
+  return (
+    <div className="">
+      <div className="supertitle mb-8">Flagstar Bank</div>
+      <div className="heading">Front-end Developer</div>
+      <div>2015&ndash;2021</div>
+      <p>I started at Flagstar as an intern, and quickly earned my stripes as the resident HTML & CSS guru.</p>
+      <p>Design system (componentization, zero dependencies (slowly migrated away from legacy version of jQuery), flexbox & modern features, site was so well tokenized we were able to stand up a website for another bank (Desert Community Bank) within a couple of weeks.</p>
+      <p>ADA compliance, WCAG level aa</p>
+      <p>Website Building tool (pre webflow and framer). components, layouts, drag n drop, style variants, etc etc.</p>
+      <p>Branch & ATM locator, Localization (translation & geo-specific mortgage rates), loan officer profile pages & personalized marketing materials, live chat, site search, and a whole lot more.</p>
+      <p>worked closely with very experienced, tight-knit team. high output. learned a ton about agile and scrum.</p>
+      <p>https://web.archive.org/web/20181227102529/https://www.flagstar.com/</p>
+      <p>https://web.archive.org/web/20180604103204/https://dcbk.org//</p>
+    </div>
+  )
+}
